f8097a8b99373e75487bbdba33dbd898c9353493
ChocoChild <nijatgasimzada@gmail.com>
2023-06-19 23:30:35 +0400

    Верстка и разбивка на компоненты

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SneakersItem from './components/SneakersItem';
+import Box from './components/Box';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper clear">
+      <Box />
+      <Header />
+      <div className='content p-40'>
+        <div className='mb-40 d-flex justify-between align-center'>
+          <h1>Все кроссовки</h1>
+          <div className='search-block d-flex'>
+            <img src='/img/search.svg' alt='search' />
+            <input placeholder='Поиск...' />
+          </div>
+        </div>
+
+        <div className='sneakers d-flex'>
+          <SneakersItem />
+        </div>
+
+      </div>
     </div>
   );
 }
